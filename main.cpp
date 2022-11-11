6e0fdce42e1209a926ed1019d1c80925812ef834
LAPTOP-VSC97D4H\Novik <Novikov.3.14@yandex.ru>
2022-11-11 16:24:02 +0300

    add first simple program

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+#include <string>
+
+int main () {
+
+    std::string s;
+
+    std::cin >> s;
+    std::cout << s << " " << s;
+    std::cin >> s;
+    std::cout << "end";
+
+    return 0;
+}
